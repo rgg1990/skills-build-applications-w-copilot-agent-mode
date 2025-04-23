56497b4180ad9d588c31cf31048b6915ebbeeb44
rgg1990 <rgg.1990@gmail.com>
2025-04-23 03:42:42 +0000

    Added djongo to settings.py and populated the database correctly.

---
+++ octofit-tracker/backend/octofit_tracker/management/commands/__init__.py
@@ -0,0 +1,1 @@
+# This file makes the commands directory a Python package and allows custom management commands to be registered.
--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "octofit_tracker",
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,10 @@ WSGI_APPLICATION = "octofit_tracker.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "djongo",
+        "NAME": "octofit_db",
+        "HOST": "localhost",
+        "PORT": 27017,
     }
 }
 
